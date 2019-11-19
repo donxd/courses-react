9e4a256a644d968e68f7ba01eab82364593c77cb
donxd <n0s3.xd@gmail.com>
2019-11-19 11:04:11 -0600

    Updates

---
+++ precache-manifest.29e298fa840a647ebd21e3a8c0e67354.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d06de3134f88dacf5c669a397b4fe795",
+    "url": "/courses-react/index.html"
+  },
+  {
+    "revision": "4b17187e907f178e7ad8",
+    "url": "/courses-react/static/css/main.f2d97305.chunk.css"
+  },
+  {
+    "revision": "d18793cb04c04e32d58b",
+    "url": "/courses-react/static/js/2.87a4944b.chunk.js"
+  },
+  {
+    "revision": "4b17187e907f178e7ad8",
+    "url": "/courses-react/static/js/main.4becbbec.chunk.js"
+  },
+  {
+    "revision": "24cf22dfcda9187a3314",
+    "url": "/courses-react/static/js/runtime-main.71ceb353.js"
+  }
+]);
